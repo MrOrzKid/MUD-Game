93f2d22264655b733b736a8a6899cd2e9f3461d6
MrOrzKid <MrOrzKid@yahoo.co.uk>
2017-04-11 21:29:04 +0100

    first commit

---
+++ MUD Question Version.rkt
@@ -0,0 +1,196 @@
+#lang racket
+
+(require srfi/1)
+(require srfi/13)
+(require srfi/48)
+
+;------------------------------------------------------------------------------------------
+
+;; User Objects
+(define objects '((1 "a silver dagger")
+                  (1 "a gold coin")))
+
+;; User Locations
+(define descriptions '((1 "You are in the lobby.")
+                       (2 "You are in the hallway.")
+                       (3 "You are in a swamp.")))
+
+;------------------------------------------------------------------------------------------
+
+;; User options
+(define look '(((directions) look) ((look) look) ((examine room) look)))
+(define quit '(((exit game) quit) ((quit game) quit) ((exit) quit) ((quit) quit)))
+(define pick '(((get) pick) ((pickup) pick) ((pick) pick)))
+(define put '(((put) drop) ((drop) drop) ((place) drop) ((remove) drop)))
+(define inventory '(((inventory) inventory) ((bag) inventory)))
+(define actions `(,@look ,@quit ,@pick ,@put ,@inventory))
+
+;; User decessions
+(define decisiontable `((1 ((north) 2) ((north west) 3) ,@actions)
+                        (2 ((south) 1) ,@actions)
+                        (3 ,@actions)))
+
+;------------------------------------------------------------------------------------------
+
+;; Objects and Inventory database
+(define objectdb (make-hash))
+(define inventorydb (make-hash))
+
+ ; Adding Object 
+(define (add-object db id object)
+  (if (hash-has-key? db id)
+      (let ((record (hash-ref db id)))
+        (hash-set! db id (cons object record)))
+      (hash-set! db id (cons object empty))))
+
+(define (add-objects db)
+  (for-each
+   (lambda (r) 
+     (add-object db (first r) (second r))) objects))
+
+(add-objects objectdb)
+
+ ; Display Object 
+(define (display-objects db id)
+  (when (hash-has-key? db id)
+    (let* ((record (hash-ref db id))
+           (output (string-join record " and ")))
+      (when (not (equal? output ""))
+        (if (eq? id 'bag)
+            (printf "You are carrying ~a.\n" output)
+            (printf "You can see ~a.\n" output))))))
+
+ ; Remove Object (from room)
+(define (remove-object-from-room db id str)
+  (when (hash-has-key? db id)
+    (let* ((record (hash-ref db id))
+           (result (remove (lambda (x) (string-suffix-ci? str x)) record))
+           (item (lset-difference equal? record result)))
+      (cond ((null? item) 
+             (printf "I don't see that item in the room!\n"))
+            (else
+             (printf "Added ~a to your bag.\n" (first item))
+             (add-object inventorydb 'bag (first item))
+             (hash-set! db id result))))))
+
+ ; Remove Object (from inventory)
+(define (remove-object-from-inventory db id str)
+  (when (hash-has-key? db 'bag)
+    (let* ((record (hash-ref db 'bag))
+           (result (remove (lambda (x) (string-suffix-ci? str x)) record))
+           (item (lset-difference equal? record result)))
+      (cond ((null? item)
+             (printf "You are not carrying that item!\n"))
+            (else
+             (printf "Removed ~a from your bag.\n" (first item))
+             (add-object objectdb id (first item))
+             (hash-set! db 'bag result))))))
+
+ ; Pick-up Item
+(define (pick-item id input)
+  (let ((item (string-join (cdr (string-split input)))))
+    (remove-object-from-room objectdb id item)))
+
+ ; Put-down Item
+(define (put-item id input)
+  (let ((item (string-join (cdr (string-split input)))))
+    (remove-object-from-inventory inventorydb id item)))
+
+(define (display-inventory)
+  (display-objects inventorydb 'bag))
+
+(define (slist->string l)
+  (string-join (map symbol->string l)))
+
+;------------------------------------------------------------------------------------------
+
+;; Directions available 
+(define (get-directions id)
+  (let ((record (assq id decisiontable)))
+    (let* ((result (filter (lambda (n) (number? (second n))) (cdr record)))
+           (n (length result)))
+      (cond ((= 0 n)
+             (printf "Oh? this room has no exits.\n"))
+            ((= 1 n)
+             (printf "There seems to be an exit to the ~a.\n" (slist->string (caar result))))
+            (else
+             (let* ((losym (map (lambda (x) (car x)) result))
+                    (lostr (map (lambda (x) (slist->string x)) losym)))
+               (printf "There seems to be exits to the ~a.\n" (string-join lostr " and "))))))))
+
+(define (assq-ref assqlist id)
+  (cdr (assq id assqlist)))
+
+(define (assv-ref assqlist id)
+   (cdr (assv id assqlist)))
+
+(define (get-description id)
+  (car (assq-ref descriptions id)))
+
+(define (get-keywords id)
+  (let ((keys (assq-ref decisiontable id)))
+    (map (lambda (key) (car key)) keys)))
+
+;; Outputs a list
+(define (list-of-lengths keylist tokens)
+  (map 
+   (lambda (x)
+     (let ((set (lset-intersection eq? tokens x)))
+       (* (/ (length set) (length x)) (length set))))
+   keylist))
+
+(define (index-of-largest-number list-of-numbers)
+  (let ((n (car (sort list-of-numbers >))))
+    (if (zero? n)
+      #f
+      (list-index (lambda (x) (eq? x n)) list-of-numbers))))
+
+(define (lookup id tokens)
+  (let* ((record (assv-ref decisiontable id)) ;changed assv to assq
+         (keylist (get-keywords id))
+         (index (index-of-largest-number (list-of-lengths keylist tokens))))
+    (if index 
+      (cadr (list-ref record index))
+      #f)))
+
+(define (display-description id)
+  (printf "~a\n" (get-description id)))
+
+;------------------------------------------------------------------------------------------
+
+;; Start Game
+(define (startgame initial-id)
+  (let loop ((id initial-id) (description #t))
+    (when description
+      (display-description id)
+      (display-objects objectdb id))
+    (printf "> ")
+    (let* ((input (read-line))
+           (string-tokens (string-tokenize input))
+           (tokens (map string->symbol string-tokens)))
+      (let ((response (lookup id tokens)))
+        (cond ((number? response)
+               (loop response #t))
+              ((eq? #f response)
+               (printf "Invalid input\n")
+               (loop id #f))
+              ((eq? response 'look)
+               (get-directions id)
+               (loop id #t))
+              ((eq? response 'pick)
+               (pick-item id input)
+               (loop id #f))
+              ((eq? response 'drop)
+               (put-item id input)
+               (loop id #f))
+              ((eq? response 'inventory)
+               (display-inventory)
+               (loop id #f))              
+              ((eq? response 'quit)
+               (printf "Thanks for playing!\n")
+               (exit)))))))
+
+;------------------------------------------------------------------------------------------
+
+;; Output
+(startgame 1)
